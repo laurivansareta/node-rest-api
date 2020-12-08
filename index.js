2312127543e4e00b5ea5790a80cd75753e727000
Laurivan <laurivansareta@gmail.com>
2020-12-07 22:31:26 -0300

    Aula 02.02

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-const express = require('express')
+const customExpress = require('./config/customExpress')
 
-const app = express()
+const app = customExpress()
 
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000')
 })
-
-app.get('/', (req, res) => res.send('Servidor rodando, tudo ok'))
-
-app.get('/atendimentos', (req, res) => res.send('Servidor rodando... Rota atendimentos'))
