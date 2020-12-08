e5abafdddfb0dde9d8893b206433f19e7e50ee89
Laurivan <laurivansareta@gmail.com>
2020-12-07 23:21:35 -0300

    Aula 03.02

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # node-rest-api
 Alura - Formação: Node.js com Express - Curso: Rest com NodeJS: API com Express e MySQL
 
+# Conexão com Banco de dados PostgreSQL (Diferente da aula vou utilizar postgreSQL)
+host: 'localhost'
+database: 'agenda-petshop'
+user: 'postgres'
+password: 'postgres'
+port: 5432
+
 # Comandos iniciais
 npm start
--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const customExpress = require('./config/customExpress')
+const conexao = require('./infraestrutura/conexao')
+// const conexao = require('./infraestrutura/conexaoMysql') // CONECTANDO AO MYSQL
 
-const app = customExpress()
+conexao.connect(erro => {
+    if(erro){
+        console.log(erro)
+    }else{
+        console.log('Conecatado com sucesso!')
+        
+        const app = customExpress()
 
-app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000')
+        app.listen(3000, () => {
+            console.log('Servidor rodando na porta 3000')
+        })
+    }
 })
+
